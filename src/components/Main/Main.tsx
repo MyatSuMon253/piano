7ff981c70a18dbaee45aed92a782bf3ad4498e67
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-04-21 08:06:29 +0630

    feat: useAudioContext - accessing audio context from web browser

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,10 @@
+import { useAudioContext } from "../AudioContextProvider";
+import { NoAudioMessage } from "../NoAudioMessage";
+
+const Keyboard = () => <>Keyboard</>;
+
+export const Main = () => {
+  const AudioContext = useAudioContext();
+
+  return !!AudioContext ? <Keyboard /> : <NoAudioMessage />;
+};
