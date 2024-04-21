19aa6810d05a50d44a78bd37242384327cc808c3
MyatSuMon253 <myatsumon2531999@gmail.com>
2024-04-21 20:05:39 +0630

    feat: creating keys and keyboards

---
+++ src/components/Key/Key.tsx
@@ -0,0 +1,20 @@
+import { FC, FunctionComponent } from "react";
+import { NoteType } from "../../domain/note";
+import clsx from "clsx";
+import styles from "./Key.module.css";
+
+type KeyProps = {
+  type: NoteType;
+  label: string;
+  disabled?: boolean;
+};
+
+export const Key: FunctionComponent<KeyProps> = (props) => {
+  const { type, label, ...rest } = props;
+
+  return (
+    <button className={clsx(styles.key, styles[type])} type="button" {...rest}>
+      {label}
+    </button>
+  );
+};
